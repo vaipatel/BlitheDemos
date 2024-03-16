274640a87f1378b29419dbe3405a5a33ffbe78a6
Vai Patel <vpatel@harmonicmedical.com>
2024-03-15 23:00:11 -0400

    Add ShaderProgram, ImAssert and optional

---
+++ app/GLWrappers/ShaderProgram.h
@@ -0,0 +1,36 @@
+#ifndef SHADERPROGRAM_H
+#define SHADERPROGRAM_H
+
+#include "optional.hpp"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <string>
+
+/*!
+* \brief Handles the creation, binding/unbinding, uniform settings, destruction of shader programs.
+*/
+class ShaderProgram
+{
+public:
+    ShaderProgram(const std::string& _vertexPath, const std::string& _fragmentPath);
+    ~ShaderProgram();
+
+    void Bind() const;
+    void Unbind() const;
+
+    void SetUniform1f(const std::string& _name, float _value);
+    void SetUniform1i(const std::string& _name, int _value);
+    void SetUniformVec3f(const std::string& _name, const glm::vec3& _value);
+    void SetUniformMat4f(const std::string& _name, const glm::mat4& _value);
+
+private:
+    tl::optional<std::string> LoadShaderSource(const std::string& _filePath);
+    GLuint CompileShader(const std::string& _source, GLenum _shaderType);
+    GLint CheckLinkingErrors();
+    void BindCommonAttribLocations();
+
+    GLuint m_programID; //!< ID of the created shader program, obtained from OpenGL
+};
+
+
+#endif // SHADERPROGRAM_H
---
+++ app/Utils/ImAssert.h
@@ -0,0 +1,22 @@
+#ifndef IMASSERT_H
+#define IMASSERT_H
+
+#include <iostream>
+
+#ifndef NDEBUG
+    // The do-while helps convert the compound statements into a single statement. So,
+    // for eg, there are no errors with using the ASSERT in an if-else block.
+    // See https://stackoverflow.com/a/1067238
+    #define ASSERT(condition, message) \
+    do { \
+        if (! (condition)) { \
+            std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
+                      << " line " << __LINE__ << ": " << message << std::endl; \
+            std::terminate(); \
+        } \
+    } while (false)
+#else
+    #define ASSERT(condition, message) do { } while (false)
+#endif
+
+#endif // IMASSERT_H
