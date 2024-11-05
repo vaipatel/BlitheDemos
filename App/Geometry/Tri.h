1024de37e32a5e30d33db38bfd4d1eee56be104e
Vaibhav Patel <vai.pateln@gmail.com>
2024-11-05 02:00:44 -0500

    Add some funcs on Tri, to get the plane info and area
    I don't like polluting the POD-ness of the struct

--- App/Geometry/Tri.h
+++ App/Geometry/Tri.h
@@ -14,6 +14,18 @@ namespace blithe
         Vertex m_v0; //!< First vertex
         Vertex m_v1; //!< Second vertex
         Vertex m_v2; //!< Third vertex
+
+        void CalcNormalAndD(glm::vec3& _normal, float& _d) const
+        {
+            _normal = glm::cross(m_v1.m_pos - m_v0.m_pos, m_v2.m_pos - m_v0.m_pos);
+            _normal = glm::normalize(_normal);
+            _d = -glm::dot(_normal, m_v0.m_pos);
+        }
+
+        float Area() const
+        {
+            return 0.5f * glm::length(glm::cross(m_v1.m_pos - m_v0.m_pos, m_v2.m_pos - m_v0.m_pos));
+        }
     };
 }
 
